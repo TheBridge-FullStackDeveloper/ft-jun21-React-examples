593951614e15a49197025da9b314f57e2436dbda
alejandroereyesb <reyesbalejandroe@gmail.com>
2021-09-21 13:01:00 +0200

    add/react_form_refs

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './Product.css'
 
 export class Product extends Component {
 
@@ -11,13 +12,17 @@ export class Product extends Component {
     }
     
     render() {
-        const {name,desc} = this.props.info
+        const {name,desc,price,url} = this.props.info
         const empresa = this.state.empresa
         return (
             <section>
                 <h3>Producto - {name}</h3>
                 <p>{desc}</p>
                 <p>Empresa: {empresa} </p>
+                <p>Precio:{price}</p>
+                <img src={url} alt={name}/>
+                <br/>
+                <button onClick={this.props.remove}>Borrar</button>
             </section>
         )
     }
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -7,46 +7,107 @@ export class ProductList extends Component {
     constructor(props) {
         super(props)
     
+        this.url = React.createRef(); // Referencia para URL
+        this.texto = React.createRef();
+
         this.state = {
              data:{},
              "products": products // [{p1},{p2},{p3},{p4},{p5}]
         }
     }
     
-    addProduct = () => {
-        const name = prompt("Introduce nombre")
-        const desc = prompt("introduce descripción")
+    addProduct = (event) => {
+        //const name = prompt("Introduce nombre")
+        //const desc = prompt("introduce descripción")
 
-        //const name = "Cocido"
-        //const desc = "Disfruta del cocido madrileño"
-        this.setState({data:{name,desc}})
+        event.preventDefault();
+        console.log("**************************");
+        const name = event.target.elements.name.value;
+        const desc = event.target.elements.desc.value;
 
-        //Añadir producto al array
-        const newProduct = {name,desc}
-        this.setState({ products: [...this.state.products, newProduct]})
+        const price = event.target.elements.price.value;
+        //const url = event.target.elements.url.value;
+        const url = this.url.current.value; // Leer el valor de la referencia URL
+        alert(url)
+
+        if(name && desc && price && url){
+            const product = {name,desc,price,url}
+            this.setState({data:product})
+
+            //Añadir producto al array
+            const newProduct = product
+            this.setState({ products: [...this.state.products, newProduct]})
+        }
 
     }
-    removeProduct = () => {
+    removeAllProducts = () => {
         this.setState({data:{}})
-        this.setState({products:[]})
+        this.setState({products}) // Deja el estado inicial
+    }
+    removeProduct = (i) => {
+       const remainProducts = this.state.products.filter((product,j)=>j!==i);
+       this.setState({products:remainProducts})
     }
 
     paintProducts = () => {
         // [{p1},{p2},{p3},{p4},{p5}] --> this.state.products
-        return this.state.products.map((product,i)=><Product info={product} key={i}/>)
+        return this.state.products.map((product,i)=><Product info={product} key={i} remove={()=>this.removeProduct(i)}/>)
     }
+
+    handleChange = (event) => {
+        console.log(event.target.value);
+    }
+    leerDato = () => {
+        console.log(this.texto.current.value)
+    }
+
+
     render() {
+        const message = this.state.data.name?
+        <h3>Último producto añadido: {this.state.data.name}</h3>:
+        <h3>Bienvenido, añade algo</h3>
+
         return (
             <>
-                <h1>Aquí lista de productos</h1> 
-                <article>
-                    {this.paintProducts()}
-                </article>
-                <button onClick={this.addProduct}>Pulsa</button>
-                <button onClick={this.removeProduct}>Borrar todo</button>
-                <h3>Último producto añadido: {this.state.data.name}</h3>
-            </>
 
+                <input type="text" ref={this.texto}/>
+                <button onClick={this.leerDato}>Pulsa y lee</button>
+
+                <form onSubmit={this.addProduct}>
+                    <label>
+                    Nombre:
+                    <input type="text" name="name" onChange={this.handleChange} />
+                    </label>
+                    <br/>
+                    <label>
+                    Descripción:
+                    <input type="text" name="desc"/>
+                    </label><br/>
+                    <label>
+                    Precio:
+                    <input type="number" name="price"/>
+                    </label><br/>
+                    <label>
+                    URL img:
+                    <input type="url" name="url" ref={this.url}/>
+                    </label><br/>
+                    <input type="submit" value="Submit" />
+                </form>
+
+                <section>
+                    <h1>Añadir producto</h1>
+                    <button onClick={this.addProduct}>Añadir producto</button>
+                    <button onClick={this.removeAllProducts}>Borrar todo</button>
+                    {message}
+                </section>
+
+                <section>
+                    <h1>Lista de productos</h1> 
+                    <article>
+                        {this.paintProducts()}
+                    </article>
+                </section>
+            </>
         )
     }
 }
